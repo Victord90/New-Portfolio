9074d6d628822b8c6874a1d0ed169991774638d2
Victor Dominguez <vd071090@gmail.com>
2020-01-29 03:25:26 -0500

    added animate on scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,9 @@
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+AOS.init();
